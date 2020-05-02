eb4ec2c999e6049ad81aff290d7334dc827df99a
surrajj20 <shah.suraj1200@gmail.com>
2020-05-03 00:33:29 +0530

    pagination & sorting of data added

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,18 +21,32 @@ router.post('/task', auth, async (req, res) => {
 })
 
 //GET /task?completed=true
+//GET /task?limit=10&skip=20
+//GET /task?sortBy=createdAt:desc
 router.get('/task', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
 
     }
 
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+        
+    }
+
     try {
         await req.user.populate({
             path: 'task',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.task)
 
