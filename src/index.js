8571aff037bfa3d0d671d35f5ecef2c13023f124
surrajj20 <shah.suraj1200@gmail.com>
2020-05-02 14:03:24 +0530

    hiding private data

--- src/index.js
+++ src/index.js
@@ -30,13 +30,3 @@ app.listen(port, () => {
     console.log('Server is running in port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
